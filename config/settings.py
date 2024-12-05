c530957309acccd8cde37683839ba92e52f146cb
CoreNion <coreinion@gmail.com>
2024-12-05 10:37:15 +0900

    [apps] アカウント / 日記帳本体のアプリを作成

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "apps.accounts.apps.AccountsConfig",
+    "apps.diary.apps.DiaryConfig"
 ]
 
 MIDDLEWARE = [
