859c15fb0a4ef4dd47345d65479f84d2a245e742
CoreNion <coreinion@gmail.com>
2024-12-05 16:03:15 +0900

    [Diary] Entryモデルを作成

--- apps/diary/models.py
+++ apps/diary/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# 日記モデル
+class EntryModel(models.Model):
+  title = models.CharField(max_length=100)
+  content = models.TextField()
+  tags = models.JSONField(blank=True, null=True)
+  date = models.DateTimeField()
+
+  def __str__(self) -> str:
+    return self.title
