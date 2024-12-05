807259c6979a0fb1083b98f55102429da5e9beaa
CoreNion <coreinion@gmail.com>
2024-12-05 16:16:41 +0900

    [views] データベースに追加されたEntryを表示する

--- apps/diary/views.py
+++ apps/diary/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import EntryModel
 
 def index(request):
   if request.user.is_authenticated:
-    return render(request, "diary/index_home.html")
+    entry_list = EntryModel.objects.all()
+    return render(request, "diary/index_home.html", {'entry_list': entry_list})
   else:
     return render(request, "diary/index_entrance.html")
