f1e27906653fc5a2b03e77a9649e86d94e728636
CoreNion <coreinion@gmail.com>
2024-12-19 10:54:26 +0900

    [views] 記事作成画面を初回実装

--- apps/diary/views.py
+++ apps/diary/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse, reverse_lazy
+
 from .models import EntryModel
 
 def index(request):
@@ -7,3 +11,9 @@ def index(request):
     return render(request, "diary/index_home.html", {'entry_list': entry_list})
   else:
     return render(request, "diary/index_entrance.html")
+  
+class CreateEntryView(LoginRequiredMixin, CreateView):
+  template_name = "diary/editer.html"
+  model = EntryModel
+  fields = {"title", "content", "tags", "date"}
+  success_url = reverse_lazy("index")
