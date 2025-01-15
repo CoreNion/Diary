6d6eb1bee6f009f74aa2fa5ffb7210818c7b6a21
CoreNion <coreinion@gmail.com>
2025-01-15 14:50:00 +0900

    [views] UI上でのタグ付け機能を実装

--- apps/diary/views.py
+++ apps/diary/views.py
@@ -11,22 +11,42 @@ def index(request):
     return render(request, "diary/index_home.html", {'entry_list': entry_list})
   else:
     return render(request, "diary/index_entrance.html")
-  
-class CreateEntryView(LoginRequiredMixin, CreateView):
-  template_name = "diary/editor.html"
-  model = EntryModel
-  fields = {"title", "content", "tags", "date", "time"}
-  success_url = reverse_lazy("diary:home")
 
-class UpdateEntryView(LoginRequiredMixin, UpdateView):
-  template_name = "diary/editor.html"
+class EntryBaseView(LoginRequiredMixin):
+  """編集画面の基底クラス"""
+
   model = EntryModel
   fields = {"title", "content", "tags", "date", "time"}
   success_url = reverse_lazy("diary:home")
 
   def get_object(self, queryset=None):
     obj: EntryModel = super().get_object(queryset)
+
+    # 日付と時刻をHTMLが理解できる文字列に変換
     obj.date = obj.date.strftime("%Y-%m-%d")
     obj.time = obj.time.strftime("%H:%M")
 
     return obj
+  
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+
+    # データがある場合(UpdateView)のみ処理
+    if self.object:
+      # タグを変数としてテンプレートに渡す
+      context["tags"] = self.object.tags
+
+    return context
+  
+  def post(self, request, *args, **kwargs):
+    # PostされたTagたちを適切な形式(リスト)に変換する
+    request.POST = request.POST.copy()
+    request.POST["tags"] = request.POST.getlist("tags")
+
+    return super().post(request, *args, **kwargs)
+  
+class CreateEntryView(EntryBaseView, CreateView):
+    template_name = "diary/editor.html"
+
+class UpdateEntryView(EntryBaseView, UpdateView):
+    template_name = "diary/editor.html"
