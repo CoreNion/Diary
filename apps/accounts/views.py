e18fd38c158e625170af2584fea0e35f7e73dadb
CoreNion <coreinion@gmail.com>
2025-01-16 10:34:04 +0900

    [User] ユーザー登録画面を作成

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views, authenticate, login
+from django.views.generic import CreateView, UpdateView
 
+from django.urls import reverse_lazy
 from django.http import HttpResponse
+
+from .forms import CustomUserSignUpForm
 
 class LoginView(auth_views.LoginView):
   template_name = "accounts/login.html"
+
+class SignUpView(CreateView):
+  form_class = CustomUserSignUpForm
+  template_name = "accounts/signup.html"
+  success_url = reverse_lazy("diary:home")
+
+  def form_valid(self, form):
+    response = super().form_valid(form)
+
+    username = form.cleaned_data.get("username")
+    password = form.cleaned_data.get("password1")
+    user = authenticate(username=username, password=password)
+    login(self.request, user)
+    
+    return response
