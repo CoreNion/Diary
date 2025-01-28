fc29a7722339dfdbb836ca97acca5c17cd621a76
Yuta Obara <coreinion@gmail.com>
2025-01-28 16:55:36 +0900

    [editor] フォームの検証を追加

--- static/js/editor.js
+++ static/js/editor.js
@@ -34,8 +34,36 @@ document.getElementById("id_content").addEventListener("input", function (event)
   this.style.height = this.scrollHeight + "px";
 });
 
+
 // 送信ボタンの処理
 document.getElementById("submit__btn").addEventListener("click", function (event) {
+  /// フォームの検証
+  const title = document.getElementById("id_title");
+  if (!title.value.trim()) {
+    title.setCustomValidity("タイトルを入力してください");
+    title.reportValidity();
+    return;
+  } else if (title.value.length > 100) {
+    title.setCustomValidity("タイトルは100文字以内で入力してください");
+    title.reportValidity();
+    return;
+  }
+
+  const date = document.getElementById("id_date");
+  const time = document.getElementById("id_time");
+  if (!time.value.trim() || !date.value.trim()) {
+    date.setCustomValidity("日時を入力してください");
+    date.reportValidity();
+    return;
+  }
+
+  const content = document.getElementById("id_content");
+  if (!content.value.trim()) {
+    content.setCustomValidity("本文を入力してください");
+    content.reportValidity();
+    return;
+  }
+
   // 送信
   document.getElementById("editor-form").submit();
 });
